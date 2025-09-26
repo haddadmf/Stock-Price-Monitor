790a682de13cc6b00f23d785d0721bbbdadd7458
haddadmf <matt.f.haddad@gmail.com>
2025-09-26 11:28:32 -0700

    added mspr feature. working on ironing out the variable type issue

--- app.py
+++ app.py
@@ -5,11 +5,12 @@ import pandas as pd
 import os
 from dotenv import load_dotenv
 import finnhub
+from datetime import datetime, timedelta
 
 def configure():
     load_dotenv()
 
-"This function fetches the current stock price using the Finnhub API"
+# This function fetches the current stock price using the Finnhub API
 def fetch_stock_price(symbol, api_key):
     finnhub_client = finnhub.Client(api_key)
 
@@ -18,6 +19,25 @@ def fetch_stock_price(symbol, api_key):
     # us = [item for item in res['result'] if item['symbol'] == symbol]
 
     return res['c']
+
+# This function returns insider info for the given stock
+def fetch_mspr(symbol, api_key):
+    
+    finnhub_client = finnhub.Client(api_key)
+
+    today = datetime.today()
+
+    first_day = today.replace(day=1).strftime("%Y-%m-%d")
+
+    next_month = today.replace(day=28) + timedelta(days=4)
+    last_day = (next_month - timedelta(days=next_month.day)).strftime("%Y-%m-%d") 
+
+    res = finnhub_client.stock_insider_sentiment(symbol, first_day, last_day)
+
+    if res['data'] == []:
+        return False
+
+    return res['data'][-1]['mspr']
 
 
 def main():
@@ -37,7 +57,7 @@ def main():
         st.write('## Live Stock Prices: ')
 
         # Create a pandas dataframe to store stock data
-        df = pd.DataFrame({'Symbol': symbols, 'Price': [0.0] * len(symbols)})
+        df = pd.DataFrame({'Symbol': symbols, 'Price': [0.0] * len(symbols), 'Monthly Share Purchase Ratio': [0.0] * len(symbols)})
 
         # Creates space to display table
         table_placeholder = st.empty()
@@ -53,6 +73,19 @@ def main():
 
                 except Exception as e:
                     st.error(f"Error fetching price for {symbol}: {e}")
+
+                try:
+                    insider_sentiment = fetch_mspr(symbol, api_key)
+
+                    if insider_sentiment == False:
+                        # Update the existing DataFrame
+                        df.loc[df['Symbol'] == symbol, 'Monthly Share Purchase Ratio'] = "No insider sentiment available"
+                    else:
+                        # Update the existing DataFrame
+                        df.loc[df['Symbol'] == symbol, 'Monthly Share Purchase Ratio'] = insider_sentiment
+
+                except Exception as e:
+                    st.error(f"Error fetching insider sentiment for {symbol}: {e}")
 
             # Update the table in place
             table_placeholder.table(df)
